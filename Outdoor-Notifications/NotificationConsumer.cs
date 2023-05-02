14c6e10c2a59879ac606d94eaae0d5ad32519fc8
ThiagoMelo <thiago.17mel@gmail.com>
2023-05-02 16:49:46 -0300

    Mensageria para envio de notificação via email implementado

--- Outdoor-Notifications/NotificationConsumer.cs
+++ Outdoor-Notifications/NotificationConsumer.cs
@@ -9,6 +9,11 @@ using System.Text.Json.Serialization;
 using System.Net.Http.Json;
 using Newtonsoft.Json;
 
+//Quick run for docker test  -> docker run --rm -it -p 15672:15672 -p 5672:5672 --name OutdoorNotify rabbitmq:3-management
+//user: guest 
+//password: guest
+
+
 namespace Outdoor_Notifications
 {
     public static class NotificationConsumer
@@ -16,7 +21,7 @@ namespace Outdoor_Notifications
         
         public static void starQueueConsumer()
         {
-            var factory = new ConnectionFactory();
+            var factory = new ConnectionFactory();// default door -> 15672
             var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
             channel.QueueDeclare("Notifications", exclusive: false, autoDelete: false);
@@ -25,7 +30,6 @@ namespace Outdoor_Notifications
                 var body = eventArgs.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
                 var emailInfo = JsonConvert.DeserializeObject<NotificationModel>(message);
-                //getObject
                 Console.WriteLine($"Notifications message received: {message}");
                 EmailService.sendEmail(emailInfo.UserEmail, $"Novas notificações para seu post: {emailInfo.PostName}", $"Olá {emailInfo.UserName} você tem novos reviews sobre seu post!");
             };
