e85370d518b92d8599d998d3e028ec343b5615bb
Felipe Valério <felipeevalerio@outlook.com>
2023-12-04 14:07:24 -0300

    Update PingController.cs

--- OutDoor-Backend/Controllers/PingController.cs
+++ OutDoor-Backend/Controllers/PingController.cs
@@ -21,7 +21,7 @@ namespace OutDoor_Backend.Controllers
             var envName = CurrentEnvironment.EnvironmentName;
             var appName = CurrentEnvironment.ApplicationName;
 
-            return "Ok app running!! \n EnvName: " + envName + "\n AppName: " + appName;
+            return "Outdoor API running!! \n EnvName: " + envName + "\n AppName: " + appName;
         }
     }
 }
